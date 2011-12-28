18f2ae077cc01fc3722aec43f6acbd06b279fd3b
Jeff Leung <jleung@curriegrad2004.ca>
2011-12-27 23:53:18 -0800

    This is the real fix for uClibc systems when it came to a specific commit on December 16th

--- src/mod/applications/mod_spandsp/mod_spandsp_modem.h
+++ src/mod/applications/mod_spandsp/mod_spandsp_modem.h
@@ -60,13 +60,29 @@
 #include <fcntl.h>
 #include <errno.h>
 #ifndef WIN32
-#if defined (__APPLE__)
+#if defined(HAVE_BYTESWAP_H)
+#include <byteswap.h>
+#elif defined(USE_SYS_ENDIAN_H)
+#include <sys/endian.h>
+#elif defined (__APPLE__)
 #include <libkern/OSByteOrder.h>
 #define bswap_16 OSSwapInt16
 #define bswap_32 OSSwapInt32
 #define bswap_64 OSSwapInt64
+//Try this one for uClibc
+#elif defined __UCLIBC__
 #else
-#include <byteswap.h>
+#define bswap_16(value)  \
+	((((value) & 0xff) << 8) | ((value) >> 8))
+
+#define bswap_32(value)	\
+	(((uint32_t)bswap_16((uint16_t)((value) & 0xffff)) << 16) | \
+	(uint32_t)bswap_16((uint16_t)((value) >> 16)))
+
+#define bswap_64(value)	\
+	(((uint64_t)bswap_32((uint32_t)((value) & 0xffffffff)) \
+	    << 32) | \
+	(uint64_t)bswap_32((uint32_t)((value) >> 32)))
 #endif
 #include <sys/time.h>
 #include <sys/signal.h>
