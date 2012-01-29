fa53f1c409e6d3ede1a2b6df9e7c2d5581cc4889
Jeff Leung <admin@curriegrad2004.ca>
2012-01-29 13:32:18 -0800

    Fixed so OPAL builds properly now. Thanks to Brian Foster for fixing the build script

--- build/buildopal.sh
+++ build/buildopal.sh
@@ -3,17 +3,17 @@ uname -a | grep -qi bsd && MAKE=gmake || MAKE=make
 FS_DIR=`pwd`
 
 cd /root
-svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/trunk ptlib
+svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/branches/v2_8 ptlib
 cd ptlib
-./configure --prefix=/usr
+./configure --prefix=/usr --disable-openldap
 ${MAKE}
 ${MAKE} install
 
 cd ..
-svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/opal/branches/v3_6 opal 
+svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/opal/branches/v3_8 opal 
 cd opal
 export PKG_CONFIG_PATH=/usr/lib/pkgconfig 
-./configure --prefix=/usr
+./configure --prefix=/usr --disable-sip --disable-video --disable-java --disable-localspeexdsp
 ${MAKE}
 ${MAKE} install
 cd ${FS_DIR}
